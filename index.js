2ed91b5c701b71d179608a0e78af2709b1099d5b
Anton Bourtnik <anton.bourtnik@hotmail.fr>
2020-08-16 12:44:55 +0200

    Complete Readme

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const Mustache = require('mustache');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
+const moment = require('moment')
+const axios = require('axios');
 
-let DATA = {
-    name: 'Anton',
-    date: new Date().toLocaleDateString('en-GB', {
-        weekday: 'long',
-        month: 'long',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        timeZoneName: 'short',
-        timeZone: 'Europe/Stockholm',
-    }),
-};
 
-// Generate Readme with data
+(async function (){
 
-fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
-    if (err) throw err;
-    const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
-});
+    let DATA = {
+        refresh_date: moment().format("dddd, DD MMMM [at] HH:mm:ss"),
+        projects : []
+    };
+
+    let response = await axios.get('http://api.antonbourtnik.fr');
+    DATA.projects = response.data.projects;
+
+    // Generate Readme with data
+
+    await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+        if (err) throw err;
+        const output = Mustache.render(data.toString(), DATA);
+        fs.writeFileSync('README.md', output);
+    });
+
+}());
